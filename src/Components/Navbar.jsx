83be66f500e798d3e936397ad5e554d6db6e0cf5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-07 18:39:49 +0100

    Updated header.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='bg-blue-500 bg-cover h-20 '>Navbar</div>
+    <div className='bg-amber-600 bg-cover p-4 flex  items-center'>
+      
+      <div className=' rounded-lg bg-blue-700 ' >
+      
+
+      </div>
+      </div>
   )
 }
 
