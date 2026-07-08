1037b8845df08f4a2fbcc30b2e549ffe16e29a16
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-08 23:01:20 +0100

    Updated Navabr.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='bg-amber-600 bg-cover p-4 flex  items-center'>
-      
-      <div className=' rounded-lg bg-blue-700 ' >
-      
-
+    <div className="bg-blue-200 p-2  flex shadow-2xl m-4 justify-between items-center">
+      <div className=" flex justify-evenly">
+        <div className="flex bg-green-500 items-center gap-2">
+          <h1 className=" font-black text-3xl "> FRUIT JUICE</h1>
+          <div className="w-2 h-2 rounded-full bg-yellow-300"></div>
+        </div>
+        <div>
+        <a href="About Us">About Us</a></div>
+        <div> 
+        <a href=""> Our Mission </a></div>
+        <div>
+        <a href=""> Flavours </a></div>
       </div>
-      </div>
-  )
-}
+      <div className="flex justify-evenly ">
+        <div>
+          <a href=""></a>
+          
+           </div>
+        <div>
+          <a href=""> </a>
+        </div>
+        <div>
+          <button className="bg-yellow-200 flex p-3 rounded-3xl  ">
+            <h4>Juice up</h4>
 
-export default Navbar
+          </button>
+        </div>
+      </div>
+      {/* <div className=' grid gap-4 grid-cols-3' >
+        <div>
+       <h1 className='text-black text-2xl font-semibold '>FRUIT JUICE</h1>
+       <div className='w-5 h-5 bg-yellow-400 rounded-full '></div>
+        </div>
+   
+  <div>
+  <a href="" className=' cursor-pointer '></a>
+  </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Navbar;
