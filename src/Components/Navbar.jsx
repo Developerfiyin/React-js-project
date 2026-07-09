ca802879ccd2544a8433dc295e0d7e47206b11e5
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-09 23:20:59 +0100

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,45 +8,44 @@ import { IoArrowForwardOutline } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-200 p-4 rounded-2xl flex shadow-2xl m-4 justify-between items-center">
+    <div className="border-b-2 border-gray-300 p-2 rounded-2xl flex m-4 justify-between items-center">
 
-      <div className=" flex justify-evenly items=center gap-4 ">
-        <div className="flex bg-green-500 items-center gap-3 p-4">
-          <h1 className=" font-black text-3xl text-black ">FRUIT JUICE</h1>
-          <div className="w-2 h-2 rounded-full bg-yellow-300"></div>
+      <div className=" flex justify-evenly items-center gap-4 ">
+        <div className="flex items-center gap-3 p-4">
+          <h1 className=" font-black text-3xl text-bold ">FRUIT JUICE</h1>
+          <div className="w-3 h-3 rounded-full bg-yellow-300"></div>
         </div>
 
         <div>
-        <a href="#about us" className=" text-xl text-black ">About Us</a>
+        <a href="#about us" className=" text-xl font-semibold text-black ">About Us</a>
         </div>
 
         <div> 
-        <a href="#our mission" className="text-xl text-black ">Our Mission </a>
+        <a href="#our mission" className="text-xl font-semibold text-black ">Our Mission </a>
         </div>
 
         <div>
-        <a href="#flavours" className="text-xl text-black ">Flavours </a>
+        <a href="#flavours" className="text-xl font-semibold text-black ">Flavours </a>
         </div>
 
       </div>
       <div className="flex justify-evenly items-center gap-4">
         <div>
-          <a href="">
-          <FiSearch className="text-2xl"/>
+          <a href="" className="cursor-pointer">
+          <FiSearch className="text-2xl "/>
           </a>
           </div>
 
         <div>
-          <a href=""> 
-             <IoBagOutline className="text-2xl" />
+          <a href="" className="cursor-pointer"> 
+             <IoBagOutline className="text-2xl text-black" />
              </a>
         </div>
 
-        <div >
-          <button className="bg-yellow-200 flex p-2 rounded-3xl justify-between items-center gap-0.5 ">
-            <h4 className="font-semibold text-xl ">Juice up</h4>
-          <IoArrowForwardOutline className="text-xl font-bold"/>
-
+        <div  >
+          <button className="bg-yellow-300 flex p-2 rounded-3xl cursor-pointer w-40 justify-evenly items-center gap-0.5 ">
+            <h4 className="font-semibold text-xl text-center ">Juice up</h4>
+          <IoArrowForwardOutline className="text-xl text-center font-bold"/>
           </button>
         </div>
        
