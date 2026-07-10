a76e95c6d33d26ac6f7d32af308e9b1132d88f4c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-10 23:41:17 +0100

    Update header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 import Navbar from './Navbar'
 const Header = () => {
   return (
-    <div className='bg-white w-full h-screen '>
+    <div className='bg-white w-full h-screen  '>
        <Navbar/>
         Header
-        <div className='bg-green-300 h-screen '>
+        <div className='bg-green-300 h-20 mx-auto '>
+                   
+                   <h2>
+                    SAVOUR
+                   </h2>
 
         </div>
         </div>
