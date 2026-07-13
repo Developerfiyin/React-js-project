e2765743688462e916da389b616e94a2043bef88
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-13 22:18:52 +0100

    update header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,8 +7,9 @@ const Header = () => {
         Header
         <div className='bg-green-400 h-20 w-full'>
 
-                      <div>
-                        <h2 className=' '> Savour <span className='text-red-300'>the Juicy essence in </span>every juice </h2>
+                      <div className='flex justify-between '>
+                        <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
+
                       </div>
         </div>
         </div>
