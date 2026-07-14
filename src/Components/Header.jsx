1cb21811ef25a1b80682015b1a2e3d3ea4c7d254
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-14 23:51:35 +0100

    Update header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 import useState from "react"
 const Header = () => {
+  const [count,setCount] = useState(false)
   return (
     <div className='bg-white w-full h-screen  '>
        <Navbar/>
@@ -10,7 +11,7 @@ const Header = () => {
 
                       <div className='flex justify-between '>
                         <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
-<button className=' p-5 '> Login in</button>
+                    <button className=' p-2 items-center justify-center bg-blue-300 rounded-2xl mt-4 mx-2  '> Login in</button>
                       </div>
         </div>
         </div>
