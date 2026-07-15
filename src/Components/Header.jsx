59a272fb3b6d20f641e04a247c5be49c3b86ad98
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-15 22:44:54 +0100

    Update header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -28,15 +28,7 @@ const Header = () => {
 
                    </div>
 
-                   <div className='flex justify-between '>
-                  <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
-                  <button className='p-2 items-center justify-center bg-red-600 rounded-2xl mt-4 mx-2 ' onClick={increaseCount}> You have clicked {count} </button>
-                  alert("why are you");
-                   </div>
-                   //fnovnev ekn dkk 
-                   <div>
-
-                   </div>
+                   
         </div>
         </div>
         
