7b52afe58f9abbffa0c77d5cb42badb1a7fa9fef
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-15 22:06:23 +0100

    Update Navbar.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import Navbar from './Navbar'
-import useState from "react"
 const Header = () => {
-  const [count, setCount] = useState(0)
-  function increaseCount () {
-    setCount(count+1)
+
+  const [count, setCount] = useState(0);
+
+  const  increaseCount = () => {
+    return(
+    setCount(count + 1 )
+
+    )
   }
+  
   return (
     <div className='bg-white w-full h-screen  '>
        <Navbar/>
         Header
+        
         <div className='bg-green-400 h-20 w-full'>
 
                       <div className='flex justify-between '>
                        <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
-                    <button onClick={} className=' p-2 items-center justify-center bg-blue-300 rounded-2xl mt-4 mx-2  '>{count} </button>
+                    <button  className=' p-2 items-center justify-center bg-blue-300 rounded-2xl mt-4 mx-2  ' onClick={increaseCount}> You have clicked {count} </button>
                       </div>
         </div>
         </div>
+        
   )
 }
 
-export default Header
+export default Header;
