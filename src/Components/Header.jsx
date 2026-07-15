87b6bf6c5b1e7f51b6c3dd5dd0c953eae54c901d
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-15 22:43:54 +0100

    Update header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,10 +18,25 @@ const Header = () => {
         
         <div className='bg-green-400 h-20 w-full'>
 
-                      <div className='flex justify-between '>
-                       <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
-                    <button  className=' p-2 items-center justify-center bg-blue-300 rounded-2xl mt-4 mx-2  ' onClick={increaseCount}> You have clicked {count} </button>
-                      </div>
+                  <div className='flex justify-between '>
+                  <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
+                  <button className='p-2 items-center justify-center bg-red-600 rounded-2xl mt-4 mx-2 ' onClick={increaseCount}> You have clicked {count} </button>
+                  alert("why are you");
+                   </div>
+                   //fnovnev ekn dkk 
+                   <div>
+
+                   </div>
+
+                   <div className='flex justify-between '>
+                  <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
+                  <button className='p-2 items-center justify-center bg-red-600 rounded-2xl mt-4 mx-2 ' onClick={increaseCount}> You have clicked {count} </button>
+                  alert("why are you");
+                   </div>
+                   //fnovnev ekn dkk 
+                   <div>
+
+                   </div>
         </div>
         </div>
         
