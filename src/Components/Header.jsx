0f0582ee203bb3d5e64e11cf725bf552c3c8ae57
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-17 21:18:09 +0100

    Update  variable to store an object

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,13 +3,11 @@ import Navbar from './Navbar'
 const Header = () => {
 
   const [count, setCount] = useState(0);
-const [fruits, setFruits] = useState(["Apple",
-"Banana",
-"Orange",
-"Grapes",
-"Mango",
-"Pineapple",
-"Watermelon"]) 
+const [search, setSearch] = useState([ "Apple", "Banana", "Orange", "Grapes", "Mango", "Pineapple", "Watermelon"]);
+
+ function searchInput() {
+   setCount(...search, [])
+ }
 
   const  increaseCount = () => {
     return(
@@ -26,7 +24,6 @@ const [fruits, setFruits] = useState(["Apple",
                   <div className='flex justify-between '>
                   <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
                   <button className='p-2 items-center font-semibold justify-center bg-red-600 rounded-2xl mt-4 mx-2 hover:bg-red-500 ' onClick={increaseCount}> You have clicked this {count} </button>
-                   alert("why are you"); 
                    </div>
                    <div className='' >
 
