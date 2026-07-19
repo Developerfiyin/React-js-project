a4649cc487f2508970ef515a1b4febc1cc0c5df1
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-19 23:34:15 +0100

    Updated Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,15 +3,18 @@ import Navbar from './Navbar'
 const Header = () => {
 
   const [count, setCount] = useState(0);
-const [search, setSearch] = useState([{
-  id:1,name: "Apple"},
-
- { id: 2, name: "Banana"},
-  {id: 3, name:"Orange"},
-   {id: 4, name:"Grapes"},
-    {id:5, name:"Mango"}, 
-    {id: 6, name: "Pineapple"},
+  const [fruit, setFruit] = useState([{
+      id:1,name: "Apple"},
+      { id: 2, name: "Banana"},
+      {id: 3, name:"Orange"},
+     {id: 4, name:"Grapes"},
+      {id:5, name:"Mango"}, 
+       {id: 6, name: "Pineapple"},
      {id:7, name: "Watermelon"}]);
+
+       const [newFruit, setNewFruit] = useState("");
+
+
 
  function searchInput() {
    setCount(...search, [])
@@ -31,15 +34,13 @@ const [search, setSearch] = useState([{
 
                   <div className='flex justify-between '>
                   <h2 className='text-4xl font-bold text-black'> Savour <span className='text-red-300 text-2xl'>the Juicy essence in </span>every juice </h2>
-                  <button className='p-2 items-center font-semibold justify-center bg-red-600 rounded-2xl mt-4 mx-2 hover:bg-red-500 ' onClick={increaseCount}> You have clicked this {count} </button>
+                <button className='p-2 items-center font-semibold justify-center bg-red-600 rounded-2xl mt-4 mx-2 hover:bg-red-500 ' onClick={increaseCount}> You have clicked this {count} </button>
                    </div>
-                   <div className='' >
+                   <div className=' bg-blue-400 w-1/2 '  >
 
-                   <h1>SANTA is Coming Home. </h1>
+                   <h1 className=' text-2xl font-semibold '>SANTA is Coming Home.</h1>
 
                    </div>
-
-
 
         </div>
         </div>
