3701bd8aa34c08b44f87172f98dbce9c55a2ed7b
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-28 12:17:03 +0100

    Updated hero.jsx

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <div>
-
-
-        <div className='flex justify-between'>
-
-<div> 
-    <h1>  <strong> Discover </strong> our range</h1>
-</div>
-<div></div>
-
+      <div className="flex justify-between">
+        <div>
+          <h1>
+            {" "}
+            <strong> Discover </strong> our range
+          </h1>
         </div>
+        <div></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
