6c1c851e10b3b66bc4d3c7e422e5fe52a98b7847
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-29 20:49:14 +0100

    Update hero.jsx

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -3,17 +3,24 @@ import React from "react";
 const Hero = () => {
   return (
     <div>
-      <div className="flex justify-between">
+      <div className="flex justify-between m-4  ">
         <div>
           <h1>
-          
-            <strong> Discover </strong> our range 
+            <strong> Discover </strong> our range
           </h1>
         </div>
-        <div>
-          <button className=" bg-white border-gray-500 p-2 "> Best seller</button>
-          <button className=" bg-white border-gray-500 p-2 "> Single Order</button>
-          <button className=" bg-white border-gray-500 p-2 ">Weekly Order </button>
+        <div className="bg-red-8 bg-black ">
+          <button className=" bg-white border-gray-600 p-2 text-black font-semibold">
+            {" "}
+            Best seller
+          </button>font-semibold
+          <button className=" bg-white border-gray-600 p-2 text-black font-semibold">
+            {" "}
+            Single Order
+          </button>
+          <button className=" bg-white border-gray-600 p-2 text-black font-semibold">
+            Weekly Order{" "}
+          </button>
         </div>
       </div>
     </div>
