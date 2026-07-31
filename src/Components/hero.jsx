36a5b6d00daa38d786820054b7dad43b25b4806c
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-31 19:52:43 +0100

    Updated hero.jsx

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import image1 from "../assets/stawberryand milk.jpg"
+import image1 from "../assets/stawberryand milk.jpg";
 
 const Hero = () => {
   return (
@@ -11,7 +11,7 @@ const Hero = () => {
           </h1>
         </div>
         <div className=" flex gap-3">
-          <button className=" border-2 border-b-gray-800 p-2 text-black font-semibold rounded-3xl text-center ">
+          <button className="   border-2 border-b-gray-800 p-2 text-black font-semibold rounded-3xl text-center ">
             Best seller
           </button>
 
@@ -27,19 +27,17 @@ const Hero = () => {
       {/**The grid ciol div that might later be flex/ */}
 
       <div className=" flex justify-evenly gap-3 ">
-
         <div>
-          <img src="" alt="" />
-          <h3></h3>
-          <button></button>
+          <img
+            src={image1}
+            alt="the first image, this one is with strawberry and milk."
+          />
+          <h3 className="text-2xl"> All-in-one-package</h3>
+          <button> </button>
         </div>
-        <div>
-          
-          
-          </div> {/** flexed div 2 */}
-        <div></div>   {/** flexed div 3 */}
-        <div></div>   {/** flexed div 4 */}
-
+        <div></div> {/** flexed div 2 */}
+        <div></div> {/** flexed div 3 */}
+        <div></div> {/** flexed div 4 */}
       </div>
     </section>
   );
