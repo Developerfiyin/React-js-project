8eba1bf4e5eb2583c5500124d749f4fc0356ebdb
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-07-31 19:33:18 +0100

    Renamed an image file

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import image1 from "../assets/stawberryand milk.jpg"
 
 const Hero = () => {
   return (
@@ -10,24 +11,34 @@ const Hero = () => {
           </h1>
         </div>
         <div className=" flex gap-3">
-          <button className=" border-2 border-b-gray-800 p-2 text-black font-semibold rounded-3xl">
+          <button className=" border-2 border-b-gray-800 p-2 text-black font-semibold rounded-3xl text-center ">
             Best seller
           </button>
-          font-semibold
-          <button className="border-2 border-b-gray-600 p-2 text-black font-semibold rounded-3xl">
+
+          <button className="border-2 border-b-gray-600 p-2 text-black font-semibold rounded-3xl text-center ">
             Single Order
           </button>
-          <button className=" border-2 border-b-gray-600 p-2 text-black font-semibold rounded-3xl">
+          <button className=" border-2 border-b-gray-600 p-2 text-black font-semibold rounded-3xl text-center ">
             Weekly Order
           </button>
         </div>
       </div>
 
-
-      {/**The grid ciol div that migh later be flex/ */}
-
+      {/**The grid ciol div that might later be flex/ */}
 
       <div className=" flex justify-evenly gap-3 ">
+
+        <div>
+          <img src="" alt="" />
+          <h3></h3>
+          <button></button>
+        </div>
+        <div>
+          
+          
+          </div> {/** flexed div 2 */}
+        <div></div>   {/** flexed div 3 */}
+        <div></div>   {/** flexed div 4 */}
 
       </div>
     </section>
