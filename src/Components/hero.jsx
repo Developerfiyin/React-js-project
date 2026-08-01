85d5379018184c3afaedb404829b6cd8c48e28ad
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-01 22:35:39 +0100

    Renamed an image

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
           </h1>
         </div>
         <div className=" flex gap-3">
-          <button className="   border-2 border-b-gray-800 p-2 text-black font-semibold rounded-3xl text-center ">
+          <button className=" border-2 border-b-gray-800 p-2 text-black font-semibold rounded-3xl text-center ">
             Best seller
           </button>
 
@@ -26,7 +26,7 @@ const Hero = () => {
 
       {/**The grid ciol div that might later be flex/ */}
 
-      <div className=" flex justify-evenly gap-3 ">
+      <div className=" flex flex-col justify-evenly gap-3 ">
         <div>
           <img
             src={image1}
