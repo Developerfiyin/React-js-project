85f4e4b3f46f50862eb0dcfe5ea4174419540680
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-01 23:48:31 +0100

    Created a grid layout

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
             </div>
 
             <h3> Passion Fruit Guava</h3>
-            <div className="flex justify-between">
+            <div className="flex justify-between bg-yellow-300">
               <button> 1 Box 12 (pack) </button>
               <button> $24.99</button>
             </div>
