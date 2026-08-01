b7c7588396089278e2b178a7950004870eb518ed
Taiwo Peace Fiyinfoluwa <developerfiyin@gmail.com>
2026-08-01 22:40:24 +0100

    Created a grid layout

--- src/Components/hero.jsx
+++ src/Components/hero.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import image1 from "../assets/stawberryand milk.jpg";
+import image2 from "../assets/vegetable-drink.jpg";
 
 const Hero = () => {
   return (
@@ -11,14 +12,14 @@ const Hero = () => {
           </h1>
         </div>
         <div className=" flex gap-3">
-          <button className=" border-2 border-b-gray-800 p-2 text-black font-semibold rounded-3xl text-center ">
+          <button className=" border-2 border-gray-800 p-2 text-black font-semibold rounded-3xl text-center ">
             Best seller
           </button>
 
-          <button className="border-2 border-b-gray-600 p-2 text-black font-semibold rounded-3xl text-center ">
+          <button className="border-2 border-bt-gray-400 p-2 text-black font-semibold rounded-3xl text-center ">
             Single Order
           </button>
-          <button className=" border-2 border-b-gray-600 p-2 text-black font-semibold rounded-3xl text-center ">
+          <button className=" border-2 border-b-gray-400 p-2 text-black font-semibold rounded-3xl text-center ">
             Weekly Order
           </button>
         </div>
@@ -38,7 +39,20 @@ const Hero = () => {
             <h4> $24.99</h4>
           </div>
         </div>
-        <div></div> {/** flexed div 2 */}
+        <div>
+          <div>
+            <div>
+              <img src={image2} alt="" cla />
+            </div>
+
+            <h3></h3>
+            <div className="flex justify-between">
+              <button></button>
+              <button></button>
+            </div>
+          </div>
+        </div>{" "}
+        {/** flexed div 2 */}
         <div></div> {/** flexed div 3 */}
         <div></div> {/** flexed div 4 */}
       </div>
